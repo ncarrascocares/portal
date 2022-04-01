38573cab38c50c5f41f10f556117906c07072cdb
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2022-04-01 13:03:00 -0300

    carga proyecto portal

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Portal</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    
+    <div class="container-fluid">
+        <div class="row"> 
+            <div class=" col med-12 text-center">
+            <h3>Portal Web</h3> 
+            </div>
+            
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 caja col-centrar">
+              
+                
+                <form action="" method="POST" role="form">
+                    <legend>Logeate</legend>
+                
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="" placeholder="Ingresa tu usuario">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="" placeholder="Ingresa tu contraseña">
+                    </div>
+                
+                    <button type="submit" class="btn btn-primary">Ingresar</button>
+                </form>
+                
+                
+            </div>
+        </div>
+        
+    </div>
+    
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
